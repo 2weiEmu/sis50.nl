b1725a2db26a665c01ddedfef2b95e42d0adda4b
2weiEmu <saalbach.robert@outlook.de>
2023-06-08 23:37:00 +0200

    refactor: index.js started refactor

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,100 +1,76 @@
+/*
+ * CONSTANTS
+ */
+const DAYS_MAP = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+const ALL_PEOPLE = ["rick", "youri", "robert", "milan", "dag"];
+const DAY_STATES = [ "<space></space>", "X", "O", "?" ];
 
-// CONSTANT STATE
-var days_map = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
-var all_people = ["rick", "youri", "robert", "milan", "dag"]
-var day_states = [ "<space></space>", "X", "O", "?" ];
+const WS = new WebSocket("ws://173.104.143.161/ws");
+//const WS = new WebSocket("ws://127.0.0.1:8000/ws")
 
-// make sure this connects to the correct server
-var ws = new WebSocket("ws://172.104.143.161/ws")
-//var ws = new WebSocket("ws://127.0.0.1:8000/ws")
+const DOM_BODY = document.getElementsByTagName("body")[0];
 
-// CLIENT SIDE FUNCTIONS
-function changeBackgroundImage(event) {
+/*
+ * LOCAL FUNCTIONS
+ */
+function set_reset_background(url) {
 
-	var new_background_url = document.getElementById("new_background").value;
-	var body = document.getElementsByTagName("body")[0];
+	console.log(`Setting Background Image: ${url}`);
 
-	var table = document.getElementsByTagName("table")[0];
-	var buy_list = document.getElementById("buy_list");
-
-	if (new_background_url != "") {
-		body.style.backgroundImage = `url(${new_background_url})`;
-		body.style.backgroundSize = "cover";
-
-		table.style.backgroundColor = "#ffffff0f"
-		buy_list.style.backgroundColor = "#ffffff0f"
-		localStorage.setItem("background-url", `${new_background_url}`)
-
+	if (url !== "") {
+		DOM_BODY.style.backgroundImage = `url(${url})`;
+		DOM_BODY.style.backgroundSize = "cover";
+		localStorage.setItem("background-url", `${url}`);
 	} else {
-		body.style.background = "linear-gradient(to bottom right, #000000, #1f1f1f)";
-		buy_list.style.backgroundColor = "#00000000";
-		table.style.backgroundColor = "#00000000";
-		localStorage.setItem("background-url", "")
+		DOM_BODY.style.backgroundColor = "#101010";
+		DOM_BODY.style.backgroundImage = '';
+		localStorage.setItem("background-url", "");
 	}
+}
 
+
+function change_background_image(event) {
+
+	var newBackgroundUrl = document.getElementById("new_background").value;
+	set_reset_background(newBackgroundUrl);
 	event.preventDefault();
 }
 
 
-// changing saved background image
-
-try {
-	var backgroundUrl = localStorage.getItem("background-url");
-	console.log(`background: ${backgroundUrl}`)
-	if (backgroundUrl != "") {
-
-	var body = document.getElementsByTagName("body")[0];
-
-	var table = document.getElementsByTagName("table")[0];
-	var buy_list = document.getElementById("buy_list");
-		body.style.backgroundImage = `url(${backgroundUrl})`;
-		body.style.backgroundSize = "cover";
-
-		table.style.backgroundColor = "#ffffff0f"
-		buy_list.style.backgroundColor = "#ffffff0f"
-		localStorage.setItem("background-url", `${backgroundUrl}`)
-		console.log("read and changed");
-	}
-} catch (error) {
-	console.log(error)
-	body.style.background = "linear-gradient(to bottom right, #000000, #1f1f1f)";
-	buy_list.style.backgroundColor = "#00000000";
-	table.style.backgroundColor = "#00000000";
-	localStorage.setItem("background-url", "")
-}
-
-function newModelItem(itemCount, itemName) {
+function new_model_item(itemCount, itemName) {
 	return `<div>
 		<p id="text-${itemCount}">${itemName}</p>
 		<button class="editItemButton" id="${itemCount}e" onclick="editItem(this.id)">Editen</button>
 		<button class="removeButton" id="${itemCount}" onclick="removeItemMessage(this.id)">X</button>
-		</div>`
+		</div>`;
 }
 
 
-function highlightCurrentDayRow() {
+function highlight_todays_row() {
 
-	var current_date = new Date();
-	var today = days_map[current_date.getDay()]
+	var currentDate = new Date();
+	var today = DAYS_MAP[currentDate.getDay()]
 
-	for (var i = 0; i < all_people.length; i++) {
-		var select_person = `${all_people[i]}-${today}`
-		var select_element = document.getElementById(select_person);
-		console.log(`changed ${select_person}`);
-		select_element.style.backgroundColor = '#0a3b0a90';
-		console.log('selection complete...');
+	for (var i = 0; i < ALL_PEOPLE.length; i++) {
+
+		var selectedPerson = `${ALL_PEOPLE[i]}-${today}`
+		var selectedElement = document.getElementById(selectedPerson);
+		selectedElement.style.backgroundColor = '#0a3b0a90';
+
+		console.log(`Changed for Highlight Today: ${selectedPerson}`);
 	}
 }
-highlightCurrentDayRow();
 
 
-/**
+/*
  * DATA FORMAT
  * [event]^[itemId]^[newValue]
-**/
+ */
 
-// SERVER INTERACTION FUNCTIONS
-ws.onmessage = function(event) {
+/*
+ * WEBSOCKET HANDLING
+ */
+WS.onmessage = function(event) {
 
 	var itemList = document.getElementById("item_list");
 
@@ -110,7 +86,7 @@ ws.onmessage = function(event) {
 	if (messageType == "addItem") {
 		var newListItem = document.createElement("li")
 		newListItem.setAttribute("id", `list_item-${mEffectedItem}`)
-		newListItem.innerHTML = newModelItem(mEffectedItem, mNewValue);
+		newListItem.innerHTML = new_model_item(mEffectedItem, mNewValue);
 
 		itemList.appendChild(newListItem);
 	} else if (messageType == "deleteItem") {
@@ -152,19 +128,19 @@ function editItem(itemId) {
 	var toChangeElement = document.getElementById(`text-${itemToEditId}`);
 	var newItemName = prompt("Edit Item", `${toChangeElement.innerHTML}`);
 
-	ws.send(`editItem^${itemToEditId}^${newItemName}`)
+	WS.send(`editItem^${itemToEditId}^${newItemName}`)
 }
 
 function removeItemMessage(itemId) {
 	var itemValue = document.getElementById(`text-${itemId}`).innerHTML;
-	ws.send(`deleteItem^list_item-${itemId}^${itemValue}`)
+	WS.send(`deleteItem^list_item-${itemId}^${itemValue}`)
 	console.log(`deleteItem^list_item-${itemId}^${itemValue}`);
 }
 
 function addItemMessage(event) {
 	var newItem = document.getElementById("add_item_field");
 	if (!(newItem.value === "")) {
-		ws.send(`addItem^_^${newItem.value}`);
+		WS.send(`addItem^_^${newItem.value}`);
 		console.log(`addItem^_^${newItem.value}`)
 	}
 	newItem.value="";
@@ -174,26 +150,45 @@ function addItemMessage(event) {
 function changeDayStatus(itemId) {
 	var current_day = document.getElementById(itemId);
 	var current_day_state = current_day.innerHTML;
-	var new_index = (day_states.indexOf(current_day_state) + 1) % day_states.length;
-	ws.send(`changeDay^${itemId}^${day_states[new_index]}`);
-	console.log(`changeDay^${itemId}^${day_states[new_index]}`)
+	var new_index = (DAY_STATES.indexOf(current_day_state) + 1) % DAY_STATES.length;
+	WS.send(`changeDay^${itemId}^${DAY_STATES[new_index]}`);
+	console.log(`changeDay^${itemId}^${DAY_STATES[new_index]}`)
 }
 
 function addNotice(event) {
 	
 	var newNoticeTextItem = document.getElementById("newNoticeText");
 	console.log(newNoticeTextItem)
-	ws.send(`addNotice^_^${newNoticeTextItem.value}`)
+	WS.send(`addNotice^_^${newNoticeTextItem.value}`)
 
 	newNoticeTextItem.value = "";
 
 	event.preventDefault()
-
 }
 
-ws.onopen = function() {
-	 ws.send("retrieveState^_^_") 
-};
+/*
+ * EXECUTION
+ */
+
+// Establishing websocket connection and retrieving state.
 // retrieves the most up-to-date for the given user, !!NOT BROADCAST!!
 // will be received as fragments of all possible operations, instead of as one big string
+WS.onopen = function() {
+	 WS.send("retrieveState^_^_") 
+};
+
+// Setting background image if previously saved.
+try {
+
+	var savedBackgroundUrl = localStorage.getItem("background-url");
+	set_reset_background(savedBackgroundUrl);
+	
+} catch (error) {
+
+	console.log(`No Previous Background Image: ${error}`);
+	lcoalStorage.setItem("background-url", "");
+}
+
+highlight_todays_row();
+
 
