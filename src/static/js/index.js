05ee45f6af8146d050676dedfd9a4038984662d9
2weiEmu <saalbach.robert@outlook.de>
2023-06-12 14:55:02 +0200

    fix: wrong ip for websocket connection

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -5,7 +5,7 @@ const DAYS_MAP = ["sunday", "monday", "tuesday", "wednesday", "thursday", "frida
 const ALL_PEOPLE = ["rick", "youri", "robert", "milan", "dag"];
 const DAY_STATES = [ "<space></space>", "X", "O", "?" ];
 
-let userWebSocket = new WebSocket("ws://173.104.143.161/ws");
+let userWebSocket = new WebSocket("ws://172.104.143.161/ws");
 //let userWebSocket = new WebSocket("ws://127.0.0.1:8000/ws");
 
 const DOM_BODY = document.getElementsByTagName("body")[0];
